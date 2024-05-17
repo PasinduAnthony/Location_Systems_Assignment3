e73e087d3b0703d3ee3c1051d349aeebef60754f
ivholmlu <ivholmlu@nmbu.no>
2024-05-17 20:52:32 +1200

    Working CQL filtering for singel year

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 
-const map = L.map('map').setView([-36.848450, 174.762192], 5);
+const map = L.map('map').setView([-36.848450, 174.762192], 10);
 
 	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		maxZoom: 19,
@@ -8,7 +8,8 @@ const map = L.map('map').setView([-36.848450, 174.762192], 5);
 	}).addTo(map);
 
 var wmsLayer = L.tileLayer.wms('http://localhost:8080/geoserver/webmap/wms', {
-layers: 'webmap:Forest_clearing_NZGD200_NZGTM',
-transparent: true,
-format: 'image/png'
-}).addTo(map);
+            layers: 'webmap:Forest_clearing_NZGD200_NZGTM',
+            format: 'image/png',
+            transparent: true,
+            CQL_FILTER: 'destock_yr=2020',
+        }).addTo(map);
