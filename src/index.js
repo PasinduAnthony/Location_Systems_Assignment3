807255ecf70e5c12ebb19c592ffb6d945e6dd620
ivholmlu <ivholmlu@nmbu.no>
2024-05-17 21:21:21 +1200

    Working filtering based on slider input

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var map = L.map('map').setView([-36.848450, 174.762192], 10);
 
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 
-const map = L.map('map').setView([-36.848450, 174.762192], 10);
+    var wmsLayer = L.tileLayer.wms('http://localhost:8080/geoserver/webmap/wms', {
+        layers: 'webmap:Forest_clearing_NZGD200_NZGTM',
+        format: 'image/png',
+        transparent: true,
+        CQL_FILTER: 'destock_yr=2021',
+    }).addTo(map);
 
-	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		maxZoom: 19,
-		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-	}).addTo(map);
+    var slider = document.getElementById("year-slider");
+    var sliderValue = document.getElementById("slider-value");
 
-var wmsLayer = L.tileLayer.wms('http://localhost:8080/geoserver/webmap/wms', {
-            layers: 'webmap:Forest_clearing_NZGD200_NZGTM',
-            format: 'image/png',
-            transparent: true,
-            CQL_FILTER: 'destock_yr=2020',
-        }).addTo(map);
+    slider.addEventListener("input", function() {
+        var year = this.value;
+        sliderValue.textContent = year; 
+        wmsLayer.setParams({
+            CQL_FILTER: 'destock_yr=' + year
+        });
+    });
+});
