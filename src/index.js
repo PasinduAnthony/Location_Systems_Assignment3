1f1aba6a08aca9de5bf6be309fc1b7b45cb05df3
ivholmlu <ivholmlu@nmbu.no>
2024-05-31 12:24:59 +1200

    Added map to home.html

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // IMPORTING LAYERS FROM GEOSERVER and OSM
     const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
@@ -11,6 +12,18 @@ document.addEventListener("DOMContentLoaded", function() {
         CQL_FILTER: 'destock_yr=2021',
     });
 
+    var exoticLayer = L.tileLayer.wms('http://localhost:8080/geoserver/webmap/wms', {
+        layers: 'webmap:nz-exotic-polygons-topo-150k',
+        format: 'image/png',
+        transparent: true,
+    });
+
+    var nativeLayer = L.tileLayer.wms('http://localhost:8080/geoserver/webmap/wms', {
+        layers: 'webmap:nz-native-polygons-topo-150k',
+        format: 'image/png',
+        transparent: true,
+    });
+
     var openTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
@@ -18,32 +31,38 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Creating layer groups to have several "groups" to choose from in layer selection.
     // Reality only single layer choosing, but have not found a better solution.
-
     const osmlayerGroup = L.layerGroup([osm]);
-    const wmsLayerGroup = L.layerGroup([osm, wmsLayer]);
-    const openTopoMapGroup = L.layerGroup([openTopoMap, wmsLayer]);
+    const openTopoMapGroup = L.layerGroup([openTopoMap]);
 
     var map = L.map('map', {
-        layers: [osm]}).setView([-36.848450, 174.762192], 10);
+        layers: [osm, wmsLayer]}).setView([-36.848450, 174.762192], 14);
 
     var baseLayers = {
-        'OSM(only)': osmlayerGroup,
+        'OSM': osmlayerGroup,
         'openTopoMap': openTopoMapGroup,
-        'Forest cover': wmsLayerGroup
     };
 
-    var layerControl = L.control.layers(baseLayers).addTo(map);
+    const overlays = {
+        'Exotic forest': exoticLayer,
+        'Native forest' : nativeLayer,
+        'Forest cover': wmsLayer,
+    };
 
-    var yearLabel = document.getElementById("slider-label-year");
-    var slider = document.getElementById("year-slider");
+    // Search bar
+        L.Control.geocoder({
+        geocoder: new L.Control.Geocoder.Nominatim({
+            geocodingQueryParams: {
+                "viewbox": "165.75,-47.31,179.36,-33.87",
+                "bounded": 1
+            }
+        })}).addTo(map);
+    var layerControl = L.control.layers(baseLayers, overlays).addTo(map);
+    var yearLabel = document.getElementById("slider-label");
+    var slider = document.getElementById("slider");
 
     slider.addEventListener("input", function() {
         var year = slider.value;
         yearLabel.textContent = year;
         wmsLayer.setParams({
             CQL_FILTER: 'destock_yr=' + year
-        });
-
-    
-    });
-});
+        })})});
